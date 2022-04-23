d1aec5dad41016285b5b8fa28bdd4de254a16649
Nandhana97 <87477563+Nandhana97@users.noreply.github.com>
2022-04-23 15:58:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name= "Nandhana";
+var last_name= "krishnan";
+var full_name= first_name.concat(last_name);
+console.log (full_name);
+var a= 10;
+var b= 20;
+var add= a+b;
+console.log(add);
+var sub= a-b;
+console.log (sub);
+var mul= a*b;
+console.log(mul);
+var div= a/b;
+console.log(div);
